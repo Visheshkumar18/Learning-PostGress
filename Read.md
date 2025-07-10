7fb56ebccf9c160e50942adfe3a770935819bcbb
Vishesh Kumar <mrvisheshk@gmail.com>
2025-07-10 22:28:40 +0530

    Protecting from sql injection

--- Read.md
+++ Read.md
@@ -2,5 +2,5 @@
 1-> npm init -y
 2-> npm i typescript 
 3->npx tsc --init
-4-> inside tsconfig.js file mark rootdir:"./src" and outdir:":/dist"
+4-> inside tsconfig.js file mark rootdir:"./src" and outdir:"./dist"
 5-> npm i pg @types/pg to connect with postgress database 
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,24 @@ import dotenv from "dotenv"
 import express from "express";
 const app=express();
 dotenv.config();
+app.use(express.json());
 const URL=process.env.DB_URL
 const pgClient =new Client(URL)
-const username="Virat"
-const email="virat@gmail.com"
-const password="virat123"
-const InsertQuery=`INSERT INTO users(username,email,password)VALUES('${username}','${email}','${password}');`
-const main=async()=>{
+const InsertQuery=`INSERT INTO users(username,email,password)VALUES($1,$2,$3);`
+app.post("/signup",async (req,res)=>{
+   try {
+        const { username, email, password } = req.body;
+        await pgClient.query(InsertQuery, [username, email, password]);
+        console.log("User inserted successfully");
+        res.status(201).json({ message: "User created successfully" });
+    } catch (err) {
+        console.error("Database error:", err);
+        res.status(500).json({ error: "Failed to create user " });
+    }
+})
+app.listen(3000,async()=>{
     await pgClient.connect();
-    pgClient.query(InsertQuery);
-    console.log("user insert successfully");
-}
-main();
+    console.log("Database connection establish!");
+});
+
+
